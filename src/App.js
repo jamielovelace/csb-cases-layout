69255ae417d55e536b831c545b5dc7659c55518b
Jamie Lovelace <pomponian@gmail.com>
2023-02-08 17:43:27 +0000

    resizing and overlapping works

--- src/App.js
+++ src/App.js
@@ -7,13 +7,11 @@ import NotesSidebar from "./NotesSidebar";
 import CaseSidebar from "./CaseSidebar";
 
 export default function App() {
-  const [showSidebar, setShowSidebar] = useState(true)
+  const [showSidebar, setShowSidebar] = useState(false)
   const [sidebarSize, setSidebarSize] = useState(300)
-
-  const ResizeHandle = React.forwardRef((props, ref) => {
-    const {handleAxis, ...restProps} = props;
-    return <div ref={ref} className={style.handle} {...restProps} />;
-  });
+  const [sidebarIsResizing, setSidebarIsResizing] = useState(false)
+  const [caseSidebarSize, setCaseSidebarSize] = useState(250)
+  const [caseSidebarIsResizing, setCaseSidebarIsResizing] = useState(false)
   
   return (
     <>
@@ -30,8 +28,17 @@ export default function App() {
                 size={{ width: sidebarSize, height: '100%'}} 
                 minWidth={200} 
                 maxWidth={500} 
+                enable={{ top:false, right:false, bottom:false, left:true, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false }}
+                boundsByDirection={true}
+                handleClasses={{
+                  left: `${style.handle} ${style.handleLeft} ${sidebarIsResizing && style.isResizing}`
+                }}
+                onResizeStart={() => {
+                  setSidebarIsResizing(true)
+                }}
                 onResizeStop={(e, direction, ref, d) => {
                   setSidebarSize(sidebarSize + d.width)
+                  setSidebarIsResizing(false)
                 }}
               >
                 <NotesSidebar close={setShowSidebar}/>
@@ -39,7 +46,25 @@ export default function App() {
             </div>
           )}
         <div className={style.body}>
-          <CaseSidebar className={style.caseSidebar} />
+          <Resizable
+            size={{ width: caseSidebarSize, height: '100%'}} 
+            minWidth={200}
+            maxWidth={400}
+            enable={{ top:false, right:true, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false }}
+            boundsByDirection={true}
+            handleClasses={{
+              right: `${style.handle} ${style.handleRight} ${caseSidebarIsResizing && style.isResizing}`
+            }}
+            onResizeStop={(e, direction, ref, d) => {
+              setCaseSidebarSize(caseSidebarSize + d.width)
+              setCaseSidebarIsResizing(false)
+            }}
+            onResizeStart={() => {
+              setCaseSidebarIsResizing(true)
+            }}
+          >
+            <CaseSidebar className={style.caseSidebar} />
+          </Resizable>
           <div className={style.case} />
         </div>
       </div>
